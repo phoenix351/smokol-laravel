6b669f5db7eae9828c683cd706f0c114b40df995
phoenix351 <kuat.ponimin123@gmail.com>
2024-01-16 17:27:29 +0800

    upload file frontend

--- resources/js/Forms/PemeliharaanForm.tsx
+++ resources/js/Forms/PemeliharaanForm.tsx
@@ -1,6 +1,6 @@
-import { Divider, Form, Input, message } from "antd";
+import { Button, Divider, Form, Input, Upload, message } from "antd";
 import { useEffect, useRef, useState } from "react";
-import { PlusOutlined } from "@ant-design/icons";
+import { PlusOutlined, UploadOutlined } from "@ant-design/icons";
 import axios from "axios";
 
 const PemeliharaanForm: React.FC<{
@@ -12,7 +12,29 @@ const PemeliharaanForm: React.FC<{
         wrapperCol: { span: 24 },
     };
     const [messageApi, contextHolder] = message.useMessage();
+    const [fileList,setFileList] = useState([]);
+    const [previewImage, setPreviewImage] = useState<string | ArrayBuffer|null>(null);
 
+
+    const handleChange = ({ fileList }:any) => {
+        setFileList(fileList);
+        if (fileList.length > 0) {
+            handlePreview(fileList[fileList.length - 1]); // Preview the last added file
+          } else {
+            setPreviewImage(null); // Clear preview if no files are selected
+          }
+      };
+      const handlePreview = async (file:any) => {
+        if (file.url) {
+            setPreviewImage(file.url);
+          } else {
+            const reader = new FileReader();
+            reader.onloadend = () => {
+              setPreviewImage(reader.result);
+            };
+            reader.readAsDataURL(file.originFileObj);
+          }
+      };
     return (
         <>
             <Form
@@ -72,6 +94,26 @@ const PemeliharaanForm: React.FC<{
                 >
                     <Input.TextArea />
                 </Form.Item>
+                <Form.Item {...formItemLayout}
+                    label="file"
+                    name="file">
+                    <Upload
+                    onChange={handleChange}
+                    fileList={fileList}
+                    beforeUpload={()=>false}
+                    onPreview={handlePreview}
+                    listType="picture-card">
+          <Button icon={<UploadOutlined />}>Select File</Button>
+
+                    </Upload>
+                </Form.Item>
+                {previewImage && (
+          <img
+            alt="Preview"
+            style={{ width: '100%', marginTop: '10px' }}
+            src={previewImage as string}
+          />
+        )}
             </Form>
         </>
     );
